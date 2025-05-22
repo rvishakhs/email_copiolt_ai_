ec9c59ee40f498832d895fdd5b207df1adceef7a
rvishakhs <rvishakhs@gmail.com>
2025-05-22 12:11:47 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+
+export default async function Home() {
+
+  return (
+      <h1 className="text-red-600">Hello World</h1>
+  );
+}
---
+++ src/playground.ts
@@ -0,0 +1,12 @@
+
+import { db } from "./server/db";
+
+await db.user.create({
+    data: {
+        emailAddress : "rvishakhs@gmail.com",
+        firstName : "Vishakh",
+        lastName : "R",
+    }
+})
+
+console.log("User created successfully");
