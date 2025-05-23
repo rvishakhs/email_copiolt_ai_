148554278f842588e926ef11f0a85c8472a516cb
rvishakhs <rvishakhs@gmail.com>
2025-05-23 10:13:42 +0100

    clerk automation and build

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,12 +1,9 @@
 import { log } from "console"
 import { db } from "~/server/db"
 
-// import { PrismaClient } from '@prisma/client'
-// const db = new PrismaClient();
 
 export const POST = async (req: Request) => {
     const {data} = await req.json()
-    log("Webhook data", data)
     const emailAddress = data?.email_addresses[0]?.email_address
     const firstName = data?.first_name
     const lastName = data?.last_name
